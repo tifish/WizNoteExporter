6c749437e81c3cff7bbd12c187d1aec3d16d283e
Tinyfish <tifish@gmail.com>
2026-06-14 16:52:27 +0800

    Adopt Fluent theme and follow system light/dark mode.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
+using System;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace WizNoteExporter
