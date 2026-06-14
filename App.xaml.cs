269cbdd63c9c708c91f370e20a47c8d2f48c8f33
Tinyfish <tifish@gmail.com>
2026-06-14 15:04:22 +0800

    Set console output encoding to UTF-8 to fix garbled Chinese in VSCode terminal.

--- App.xaml.cs
+++ App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -11,5 +12,12 @@ namespace WizNoteExporter
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application { }
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            base.OnStartup(e);
+        }
+    }
 }
